4642b993ff74c06dcc40ca56407c4875904e8985
louRemy <louisremy96@gmail.com>
2020-05-14 12:36:02 -0400

    setup, db config, made list of thing to impl

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,52 @@
-//to do - set up db, connect to routers
+//to do -  connect to routers
+import express from 'express';
+import cors from 'cors';
+import { Pool } from 'pg'
+const app = express();
+
+//middleware
+app.use(cors())
+app.use(express.json())
+
+
+//db config
+export const connectionPool: Pool = new Pool({
+    user:
+    password:
+    host:
+    port:
+    database:
+})
+
+//////routes//////
+
+//login
+
+//user
+//get users reimbs 
+
+//get reimb details
+
+//create a reimb
+
+//update a pending reimb
+
+//admin
+//create new user
+
+//update a user
+
+//delete a user
+
+//finance manager 
+//get all reimb
+
+//get all by type or status
+
+//view reimb details
+
+//update reimb from pending to either approved or denied 
+
+app.listen(8080, ()=>{
+    console.log('server running on port 8080')
+})
