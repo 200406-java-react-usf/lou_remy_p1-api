243b90967a5348e9480ad82aef6458b60d9871ac
louRemy <louisremy96@gmail.com>
2020-05-16 20:42:37 -0400

    basic get put post for user, post for admin and gets for finman

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
-//to do -  connect to routers
+//to do -  make rest of routes, 
+//view reimb details part of api? the query should return those details but doesnt right now,
+//start ecapsulating and abstracting logic into 
+//respective directories,
+//make authorization,
 import express from 'express';
 import cors from 'cors';
 import { Pool } from 'pg'
@@ -31,7 +35,17 @@ export const connectionPool: Pool = new Pool({
 
 //user
 //get users reimbs 
-
+app.get('/myreimb/:id', async (req,res)=>{
+    try {
+        const { id } = req.params
+        const reimbsByid = await connectionPool.query(
+            `select * from ers_reimbursements er
+            where er.author_id = $1`,[id])
+        res.json(reimbsByid.rows)
+    } catch (error) {
+        
+    }
+})
 //get reimb details
 
 //create a reimb
@@ -48,7 +62,26 @@ app.post('/myreimb',async (req,res)=>{
     }
 })
 //update a pending reimb
-
+app.put('/myreimb/:id&:reimbid', async (req,res)=>{
+    try {
+        const  id  = parseInt(req.params.id)
+        console.log(req.params.id)
+        const reimbid =parseInt(req.params.reimbid)
+        const { description, amount } = req.body
+        const updateReimb = await connectionPool.query(
+            `update ers_reimbursements er
+            set description = $1,
+            amount = $2
+            where (er.author_id = $3 and
+            er.reimb_id = $4 and
+            er.reimb_status_id =1)`, [description,amount,id,reimbid]) 
+            res.json(updateReimb)
+            console.log(updateReimb)
+    } catch (error) {
+        console.log(error.message)
+        
+    }
+})
 //admin
 //create new user
 app.post('/addusers', async (req,res)=>{
@@ -80,19 +113,25 @@ app.get('/reimb', async (req,res)=>{
     
 })
 //get all by type or status
-app.get('/reimb/:type', async (req,res)=>{
+app.get('/reimb/:typeid', async (req,res)=>{
+    //currently returning [] w postman
     try {
         const { type } = req.params
         const reimbsByTypes = await connectionPool.query(
-            "select * from ers_rimbursements where reimb_type_id = $1", [type])
+            `select * from ers_reimbursements er
+            inner join ers_reimbursement_types ert on er.reimb_type_id = ert.reimb_type_id
+            where er.reimb_type_id = $1`, [type])
         res.json(reimbsByTypes.rows)
     } catch (error) {
         
     }
 })
-//view reimb details
+//view reimb details 
 
 //update reimb from pending to either approved or denied 
+app.put
+
+
 
 app.listen(8080, ()=>{
     console.log('server running on port 8080')
